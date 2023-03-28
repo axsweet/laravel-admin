351d4436943487470cc5818d9d59afbf4ab873bf
alexander <alexander@uniquephoto.com>
2023-03-28 12:46:14 -0400

    Minor revisions pre deploy of tabs

--- src/Middleware/Authenticate.php
+++ src/Middleware/Authenticate.php
@@ -4,7 +4,6 @@ namespace Encore\Admin\Middleware;
 
 use Closure;
 use Encore\Admin\Facades\Admin;
-use Cookie;
 
 class Authenticate
 {
@@ -19,8 +18,8 @@ class Authenticate
     public function handle($request, Closure $next)
     {
         \config(['auth.defaults.guard' => 'admin']);
- 
-        $redirectTo = admin_base_path(config('admin.auth.redirect_to', 'auth/login')).'?returnUrl='.$request->url();
+
+        $redirectTo = admin_base_path(config('admin.auth.redirect_to', 'auth/login'));
 
         if (Admin::guard()->guest() && !$this->shouldPassThrough($request)) {
             return redirect()->to($redirectTo);
