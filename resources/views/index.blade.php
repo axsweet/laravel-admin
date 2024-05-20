665d2cbd3a601382fa6135fb558a5277e5ced507
alexander <alexander@uniquephoto.com>
2024-05-20 12:20:28 -0400

    upgraded JS and css for select 2 and boostrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,11 +20,6 @@
 
     <script src="{{ Admin::jQuery() }}"></script>
     {!! Admin::headerJs() !!}
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
 </head>
 
 <body class="hold-transition {{config('admin.skin')}} {{join(' ', config('admin.layout'))}}">
