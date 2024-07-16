c5a5ef08434607850ebfc8711215f4e48534ffbb
alexander <alexander@uniquephoto.com>
2024-07-16 10:19:11 -0400

    Added Ajax for editable to reload page.

--- src/Grid/Displayers/Editable.php
+++ src/Grid/Displayers/Editable.php
@@ -192,6 +192,9 @@ class Editable extends AbstractDisplayer
         } else {
             $.admin.toastr.error(response.message, '', {positionClass:"toast-top-center"});
         }
+        if (response.reload){
+            $.pjax.reload('#pjax-container');
+        }
     }
 }
 STR;
